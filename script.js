c8bed8f11742adccfbabe2ddd282f7eb401fc208
Adam Campbell <adam.campbell@ittybittyapps.com>
2021-01-13 12:24:23 +1100

    Safely set app as either the iOS handler or the Android handler

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.addEventListener("message", (event) => {
     let dataString = (typeof event.data) === 'string' ? event.data : JSON.stringify(event.data);
-    window.webkit.messageHandlers.iOS.postMessage(dataString);
+    var app
+
+    try {
+      app = window.webkit.messageHandlers.iOS;
+    } catch {}
+
+    if (typeof app === 'undefined' && typeof Android !== 'undefined') {
+      app = Android;
+    }
+
+    if (typeof app !== 'undefined') {
+      app.postMessage(dataString);
+    }
 });
 
 function postCheckoutMessage(message, targetOrigin) {
